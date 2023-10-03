b7d85af754e4388eb31041900e61164048415138
novagldev <novagl.dev@gmail.com>
2023-10-02 22:25:23 -0500

    Ultimos detalles del responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
-import cloudflare from "@astrojs/cloudflare";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()]
-  ,
-  adapter: node({
-    mode: "standalone"
-  })
 });
